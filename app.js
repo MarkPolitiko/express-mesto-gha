90d6add1d3317e026259828d7aeb17ed8a017290
Mark Politiko <politikomark@yandex.ru>
2023-01-05 23:58:06 +0300

    13th project for the 1st check

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ mongoose.set('strictQuery', true);
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(bodyParser.json());
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '63b72b4c555a554000d4a918',
+    _id: '63b738de6e94eb10ac81135d',
   };
 
   next();
